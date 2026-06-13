9e077661c55519eadcff97c0f9eae469d72b294b
Matar Mbaye <serignematarmbaye2000@gmail.com>
2026-06-13 14:44:00 +0000

    feat: auto-create superadmin if none exists

---
+++ accounts/management/commands/ensure_admin.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from decouple import config
+
+
+class Command(BaseCommand):
+    help = 'Crée un superadmin si aucun n\'existe'
+
+    def handle(self, *args, **options):
+        if User.objects.filter(is_superuser=True).exists():
+            self.stdout.write('Un superadmin existe déjà.')
+            return
+
+        username = config('ADMIN_USERNAME', default='admin')
+        email = config('ADMIN_EMAIL', default='admin@ameek.sn')
+        password = config('ADMIN_PASSWORD', default='Admin123!')
+
+        User.objects.create_superuser(username=username, email=email, password=password)
+        self.stdout.write(self.style.SUCCESS(f'Superadmin "{username}" créé avec succès.'))
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "=== Running migrations ==="
 python manage.py migrate --noinput 2>&1
+echo "=== Ensuring admin user ==="
+python manage.py ensure_admin 2>&1
 echo "=== Starting gunicorn ==="
 gunicorn amicale.wsgi --log-file -
