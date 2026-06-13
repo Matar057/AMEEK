1332f9009a61133080793563d048b7168fd5d548
Matar Mbaye <serignematarmbaye2000@gmail.com>
2026-06-13 15:20:56 +0000

    Serve media files via WhiteNoise in production

--- amicale/wsgi.py
+++ amicale/wsgi.py
@@ -1,7 +1,12 @@
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amicale.settings')
 
-application = get_wsgi_application()
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+application = WhiteNoise(get_wsgi_application())
+application.add_files(str(BASE_DIR / 'media'), prefix='media/')
