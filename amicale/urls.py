98e2ff5309fdc1b0f0309fe7baffda91af09ae88
Matar Mbaye <serignematarmbaye2000@gmail.com>
2026-06-13 15:35:24 +0000

    Serve media files unconditionally + ensure media dirs exist in repo

--- amicale/urls.py
+++ amicale/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 admin.site.site_header = 'Administration'
 admin.site.site_title = 'Administration'
@@ -21,5 +21,6 @@ urlpatterns = [
     path('communication/', include('communication.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
