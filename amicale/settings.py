de2b5ec579e955303b57995609a0641aca9dfc3e
Matar Mbaye <serignematarmbaye2000@gmail.com>
2026-06-14 22:17:16 +0000

    Change default SMTP host to Brevo (smtp-relay.brevo.com)

--- amicale/settings.py
+++ amicale/settings.py
@@ -137,7 +137,7 @@ STORAGES = {
 # Email
 # --------------------------------------------------
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.sendgrid.net')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp-relay.brevo.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
